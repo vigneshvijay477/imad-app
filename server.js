847e5ea1f008c355e8209bb74c91ce09737f65ea
vigneshvijay477 <30429935+vigneshvijay477@users.noreply.github.com>
2017-08-20 11:35:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.get('/', function (req, res) {
 
 
 
-app.get('/article one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('Article  one requested and served');
 });
-app.get('/article two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article  two requested and served');
 });
 
-app.get('/article three', function (req, res) {
+app.get('/article-three', function (req, res) {
  res.send('Article three requested and served');
 });
 
